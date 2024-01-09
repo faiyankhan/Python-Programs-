46003f431db6d5e42b0441acea4358f2be99b6f9
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-09 16:57:08 +0000

    Operators in python

---
+++ operators.py
@@ -0,0 +1,30 @@
+# Arithmatic Operators in python 
+num1 = 44
+num2 = 9
+print("num1 + num2 is ", num1 + num2)
+print("num1 - num2 is ", num1 - num2)
+print("num1 * num2 is ", num1 * num2)
+print("num1 / num2 is ", num1 / num2)
+print("num1 // num2 is ", num1 // num2)
+print("num1 ** num2 is ", num1 **num2)
+print("num1 % num2 is ", num1 % num2)
+
+# Assignment operator in python 
+a = 4
+a *=6
+print(a)
+ 
+# Comparison Operator in python 
+x = 7 
+y = 4
+z = 7
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x==z)
+
+# logical operator in python 
+print(x==z and x<y)
+print(x==z or x<y)
+print(not(False))
+print(not(True))

c3ab7e5ebd3ade17843e8068459d7fbfe2c00a08
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-09 16:54:03 +0000

    Varibles in python

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# Variables in python  
+a=8
+print(a)
+b=6
+print(b)
+a=8.8
+print(a)
+c = True
+print(c)
+d = "faiyan"
+print(d)
+e = None
+print(e)
+f = "5"
+print(int(f) + 1)
+g = 5
+print(g + 1)

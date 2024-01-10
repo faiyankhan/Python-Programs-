ea36ebdb5651616dfb2f92ca5d9fab8a92a97308
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-10 15:45:46 +0000

    Strings in python

---
+++ Strings.py
@@ -0,0 +1,17 @@
+# String method in python .
+name = 'Faiyan'
+number = "5"
+print(name)
+
+print(name[0:3])
+print(name[1:6])
+
+print(name.upper())
+print(name.capitalize())
+print(name.count("a"))
+print(name.isalnum())
+print(number.isnumeric())
+print(name.isnumeric())
+print(name.isnumeric())
+
+

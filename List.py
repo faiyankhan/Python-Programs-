3e2fd7aaf427ed58211388ce6c640fec87914db5
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-10 16:06:34 +0000

    Methods of List in Python

---
+++ List.py
@@ -0,0 +1,18 @@
+# Introduction to List in Python .
+
+L1 = [4,55,77,77,77,7,77,778]
+print(L1)
+
+print(type(L1))
+
+L1.count(55)
+print(L1)
+
+L1.sort()
+print(L1)
+
+L1.extend(L1)
+print(L1)
+
+L1.append(L1)
+print(L1)

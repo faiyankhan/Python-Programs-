88396fb2875240037db5411cdba971846d1827e9
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-10 15:41:29 +0000

    Userinput Method in python

---
+++ Userinput.py
@@ -0,0 +1,15 @@
+# Userinput method in python.
+
+name = input("Enter your Name: ")
+print(type(name))
+print(name)
+
+# Write a python program to take a number from user as a input from the User and add 6 to it .
+num = input("Enter a number: ")
+print(type(num))
+print(int(num) + 5)
+
+# Combution of Two Numbers.
+(x) = input("Enter first Number: ")
+y = input("Enter Second Number: ")
+print(x+y)

2ce096789a1f557735fd6226bd131239ea92bd8d
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-11 14:32:15 +0000

    Dictionary methods in Python

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+#Dictionary in python.
+dict = {
+     "Faiyan khan" : "Present",
+     "Abhishek Mandal": "present",
+     "Lavkush pandit": "present",
+     "Pranay pawar":"Absent"
+}
+
+print(dict["Faiyan khan"])
+
+# Dictionary methods in python.
+info = {'Name':'Abhishek Mandal' , 'Age':22 , 'Eligiblity':True}
+print(info)
+print(info.get('Name'))
+
+info = {'Name':'Abhishek Mandal' , 'Age':22 , 'Eligiblity':True}
+print(info)
+print(info.keys())
+

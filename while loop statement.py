b05380f6f3c0d8d391e8c75580f2b061b90bb90e
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-12 13:16:56 +0000

    While loop Statement in Python

---
+++ while loop statement.py
@@ -0,0 +1,13 @@
+#While Loop Statement in Python .
+i = 0
+while(i<20):
+    print(i)
+    i += 1
+print("Program has finished Executing")
+
+while(True):
+    num = int(input("Enter Number:"))
+    print(num)
+    if(num ==0):
+        break
+print("program has finish Executing")

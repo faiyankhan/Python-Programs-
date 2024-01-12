ac9bb7e2de6d2e845ca8062bb52c7bb2d8c92ddb
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-12 13:13:25 +0000

    For-Loop statement in Python

---
+++ for-loop statement.py
@@ -0,0 +1,22 @@
+# for loops in Python .
+colors = ["Red","Blue","Green","Yellow","orange","Purple"]
+for color in colors:
+    print(color)
+for i in color:
+    print(i)
+
+for k in range(1,100):
+    print(k)
+
+for i in range(5):
+    print(i+1)
+
+# Use of For loop in List .
+a = [1,2,3,4,5,6,7,8,9]     
+for item in a:
+    print(item)
+
+# Use of for loop in Sets.
+s =  {23,43,23,55,24,23,83,43}
+for item in s:
+    print(item)

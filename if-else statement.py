416648ea2eb29bdbb620370add0d1115d2bd208c
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-12 13:11:02 +0000

    If-Else statement in python

---
+++ if-else statement.py
@@ -0,0 +1,26 @@
+# if - Else statement in Python .
+age = int(input("Enter your age:"))
+
+if(age >= 20):
+    print(" Yes you are Eligible ")
+elif(age <= 18):
+    print("No you are a kid ")
+else:
+    print(" No you are not Eligible ")
+
+# If-Else Condition Statement in Python .
+appleprice = 220
+budget = 180
+if(appleprice <= budget):
+   print("Add 1kg apple to the cart")
+else:
+    print("Do not add 1kg apple to the cart")
+    
+
+num = int(input("Enter  the Number:"))
+if(num < 0 ):
+    print("Number is Negative")
+elif(num == 0):
+    print("Number is Zero")
+else:
+    print("Number is poositive")

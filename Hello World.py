b81168ebea28f13d0002813f53c59aad3d9b1944
FAIYAN KHAN <144541895+faiyankhan@users.noreply.github.com>
2024-01-19 18:00:00 +0000

    First Program in Python

--- Hello World.py
+++ Hello World.py
@@ -1,3 +1,4 @@
+# First Python Program .
 print("Hello World! ")
 print("Faiyan khan ")
 
@@ -6,3 +7,12 @@ a = 10
 b = 10
 c = a + b 
 print(c)
+
+#This is traditional method
+name = "Bro"
+age = 18
+attractive = True
+
+print(name)
+print(age)
+print(attractive)

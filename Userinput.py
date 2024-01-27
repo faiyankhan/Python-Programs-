ff44df4b4c67ea47211ffdfb0e665ed9b8fdf1ce
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-01-27 16:08:56 +0000

    Taking Userinput from user

--- Userinput.py
+++ Userinput.py
@@ -10,6 +10,16 @@ print(type(num))
 print(int(num) + 5)
 
 # Combution of Two Numbers.
-(x) = input("Enter first Number: ")
-y = input("Enter Second Number: ")
+x = int(input("Enter first Number: "))
+y = int(input("Enter Second Number: "))
 print(x+y)
+
+a = int(input("Enter A :"))
+b = int(input("Enter B :"))
+c = int(input("Enter C :"))
+s = (a+b+c)/2
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+p = a+b+c
+print("Area =",area)
+print("perimeter =",p)
+
